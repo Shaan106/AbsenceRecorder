a9d3f4c54712dc8238752e01309f2c2fd0031406
Shaan <shaan@yadu.in>
2021-03-01 16:25:20 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Aasmaan Yadav on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
